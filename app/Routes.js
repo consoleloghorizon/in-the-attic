8761456b110bee944dd686e90e093c8d570af800
Darien Foster <dfoster@synergybis.com>
2019-01-26 22:05:13 -0500

    Display wip

--- app/Routes.js
+++ app/Routes.js
@@ -4,12 +4,14 @@ import routes from './constants/routes';
 import App from './containers/App';
 import TitlePage from './containers/TitlePage';
 import LobbyPage from './containers/LobbyPage';
+import DisplayPage from './containers/DisplayPage';
 
 export default () => (
   <App>
     <Switch>
       <Route path={routes.LOBBY} component={LobbyPage} />
       <Route path={routes.TITLE} component={TitlePage} />
+      <Route path={routes.DISPLAY} component={DisplayPage} />
     </Switch>
   </App>
 );
