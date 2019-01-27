39d370ddf63684587b5df377ec4592366ab3f779
Darien Foster <dfoster@synergybis.com>
2019-01-27 14:46:26 -0500

    point to remote server

--- app/constants/config.js
+++ app/constants/config.js
@@ -1,4 +1,4 @@
 export const SERVER_PROTOCOL = 'http';
-export const SERVER_HOST = 'localhost';
+export const SERVER_HOST = 'ec2-3-92-201-176.compute-1.amazonaws.com';
 export const SERVER_PORT = 3000;
 export const SERVER_URL = `${SERVER_PROTOCOL}://${SERVER_HOST}:${SERVER_PORT}`;
